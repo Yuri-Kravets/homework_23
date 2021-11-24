056348e1679b2095c693f44c2148dca9b4c8e8b9
Yuri-Kravets <kravets.yuri.v@gmail.com>
2021-11-24 15:09:50 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,36 @@
+/*(function(arg) {
+  let a = arg;
+    function sum(value) {
+
+
+    }
+}
+
+)();
+*/
+(function () {
+  function sum (value) {
+      let temp = value;
+      sum = function() {
+          let arg = arguments[0] ? arguments[0] : 0;
+          return temp += arg;
+      };
+      return sum();
+  }
+  console.log(sum(3)); //3
+  console.log(sum(8)); //11
+  console.log(sum(4)); //15
+})();
+
+
+
+
+
+
+/*function func1(a, b, c) {
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+  console.log(arguments[2]);
+}
+  func1('fgfgf',34,'qwerty');
+  */
